e7a8c0f426706d5d3bbc5d4fa60667beac81adcd
recipra <jmo0425@outlook.com>
2022-06-15 17:03:11 -0400

    add skills path

---
+++ data/skills-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {text: 'Turn the computer on', mastered: false},
+  {text: 'JavaScript', mastered: false},
+  {text: 'CSS', mastered: false},
+  {text: 'Sandwich-making', mastered: true}
+]
+
+export {
+  skills
+}
