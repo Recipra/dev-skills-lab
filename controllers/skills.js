e3298cf005963c7b346f5c19458a68eb5d356c57
recipra <jmo0425@outlook.com>
2022-06-16 13:30:05 -0400

    add delete functionality and add basic viewer friendly styling

--- controllers/skills.js
+++ controllers/skills.js
@@ -67,11 +67,23 @@ function edit(req, res) {
   })
 }
 
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
   newSkill as new,
   create,
   show,
   edit,
-  update
+  update,
+  deleteSkill as delete
 }
--- routes/skills.js
+++ routes/skills.js
@@ -10,6 +10,7 @@ router.get('/:id', skillsCtrl.show)
 router.post('/', skillsCtrl.create)
 router.get('/:id/edit', skillsCtrl.edit)
 router.put('/:id', skillsCtrl.update)
+router.delete('/:id', skillsCtrl.delete)
 
 export {
   router
